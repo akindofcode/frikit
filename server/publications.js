933f85a9f7815379ba5ceefb2bd112ed1d3c2744
akindofcode <akindofcode@gmail.com>
2014-01-15 23:35:11 +0100

    v0.04 implementado iron-router, layout.html

--- server/publications.js
+++ server/publications.js
@@ -1,5 +1,14 @@
-// Vamos a publicar los datos que queremos que lleguen al cliente
+// Vamos a publicar los datos que queremos que lleguen al cliente, este ha de subscribirse en /client/main.js
 
 Meteor.publish('posts', function() {
 	return Posts.find();
 });
+
+// Por ejemplo si queremos publicar solo parte de una colección, podríamos hacer:
+// Meteor.publish('somePosts', function() {
+//  	return Posts.find({'author':'Tom'});
+// });
+// Si queremos excluir algunos campos, hacemos:
+// Meteor.publish('somePosts', function() {
+//  	return Posts.find({'author':'Tom'}, {fields: {date:false}});
+// });
