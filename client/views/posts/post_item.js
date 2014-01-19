b99b4df846ba801a9c8f7ddf760091c0c8b90a89
akindofcode <akindofcode@gmail.com>
2014-01-19 19:10:59 +0100

    v0.13 Retocados formulario nuevo mensaje y listado mensajes

--- client/views/posts/post_item.js
+++ client/views/posts/post_item.js
@@ -14,3 +14,17 @@ Template.postItem.helpers({
 //		return Comments.find({postId: this._id}).count();
 //	}
 });
+
+Template.postItem2.helpers({   //manera temporal de quitar el boton de comentar cuando ya estamos en comentarios
+	ownPost: function() {
+		return this.userId == Meteor.userId();
+	},
+	domain: function() {
+	var a = document.createElement('a');
+	a.href = this.url;
+	return a.hostname;
+	},
+	submittedText: function() {
+	return new Date(this.submitted).toLocaleString();
+	}
+});
